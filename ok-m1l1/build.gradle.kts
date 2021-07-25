12d4afac4d5bde216b685e8b097007184000beeb
Ростислав Абалов <gasick@localhost.localdomain>
2021-07-25 16:56:45 +0300

    Вынести все версии библиотек в файл конфигурации.

--- ok-m1l1/build.gradle.kts
+++ ok-m1l1/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+
+
 
 dependencies {
     implementation(kotlin("stdlib"))
---
+++ ok-m1l7-kmp/src/commonMain/kotlin/mail.kt
@@ -0,0 +1,4 @@
+package ru.otus.otuskotlin.orchestrator.kmp
+fun main() {
+    println("Hello world!")
+}
